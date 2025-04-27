b45163d2d2e950fbbd82650a50d7fd15adc7e32a
Ing. Jorge Luis Mayorga Taborda <jl.mayorga.co@gmail.com>
2025-04-27 12:00:09 -0500

    Modify `GenresConfig.py` to enhance performance on April 27, 2025

---
+++ config/GenresConfig.py
@@ -0,0 +1,25 @@
+# src/domain/classes/genres.py
+
+from enum import Enum
+
+class GenresEnum(Enum):
+    ACTION = 'Action'
+    ADVENTURE = 'Adventure'
+    ANIMATION = 'Animation'
+    BIOGRAPHY = 'Biography'
+    COMEDY = 'Comedy'
+    CRIME = 'Crime'
+    DOCUMENTARY = 'Documentary'
+    DRAMA = 'Drama'
+    FAMILY = 'Family'
+    FANTASY = 'Fantasy'
+    HISTORY = 'History'
+    HORROR = 'Horror'
+    MUSIC = 'Music'
+    MYSTERY = 'Mystery'
+    ROMANCE = 'Romance'
+    SCIENCE_FICTION = 'Science Fiction'
+    SPORT = 'Sport'
+    THRILLER = 'Thriller'
+    WAR = 'War'
+    WESTERN = 'Western'
